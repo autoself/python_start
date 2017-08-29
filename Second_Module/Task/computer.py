0bf556a1cb5d981f9c0c6bd349990ab95f177cd8
autoself <459804062@qq.com>
2017-08-29 19:06:49 +0800

    seconde v3

--- Second_Module/Task/computer.py
+++ Second_Module/Task/computer.py
@@ -4,49 +4,113 @@
 __author__ = 'andylin'
 __date__ = '17-8-17 下午10:02'
 
-
 import re
-import operator
 
 
 
+import  re
+
+def common_sub(args):
+    '''
+    计算结果
+    :param args:
+    :return:
+    '''
+    while True:
+        if args.__contains__('+-') or args.__contains__('--') or args.__contains__('++') or args.__contains__('-+'):
+            args = args.replace('+-', '-')
+            args = args.replace('++', '+')
+            args = args.replace('-+', '-')
+            args = args.replace('--', '+')
+        else:
+            break
+    check_status = re.search('[\+\-]?\d+\.?\d*[\*\/]{1}[\+\-]?\d+\.?\d*',args)
+    if not check_status:
+        return args
+    str_sub = check_status.group()
+    check_strs = list(str_sub.rpartition('*'))
+    check_len = [i for i in check_strs if i != '']
+    if len(check_len) != 3:
+        check_strs = list(str_sub.rpartition('/'))
+    if check_strs[1] == '*':
+        value = float(check_strs[0]) * float(check_strs[2])
+    else:
+        value = float(check_strs[0]) / float(check_strs[2])
+    #after = re.split('[\+\-]?\d+\.?\d*[\*\/]{1}[\+\-]?\d+\.?\d*', args, 1)
+    if value > 0:
+        value = '+%s' % (str(value))
+    args = args.replace(str_sub, str(value))
+    return common_sub(args)
+
+
+def common_add_sub(args):
+    '''
+    计算加减
+    :param args:
+    :return:
+    '''
+    while True:
+        if args.__contains__('+-') or args.__contains__('--') or args.__contains__('++') or args.__contains__('-+'):
+            args = args.replace('+-', '-')
+            args = args.replace('++', '+')
+            args = args.replace('-+', '-')
+            args = args.replace('--', '+')
+        else:
+            break
+    check_status = re.search('[\+\-]?\d+\.?\d*[\+|\-]{1}\d+\.?\d*',args)
+    if not check_status:
+        return  args
+    str_sub = check_status.group()
+    check_strs = list(str_sub.rpartition('-'))
+    check_len = [ i for i in check_strs if i !='' ]
+    if len(check_len) != 3:
+        check_strs = list(str_sub.rpartition('+'))
+    if  check_strs[1] == '+' :
+        value = float(check_strs[0])+float(check_strs[2])
+    else:
+        value = float(check_strs[0])-float(check_strs[2])
+    #after  = re.split('[\+\-]?\d+\.?\d*[\+|\-]{1}\d+\.?\d*',args,1)
+    args = args.replace(str_sub,str(value))
+    return common_add_sub(args)
 
 
 
+def common_all_div(arg):
+    '''
+    计算加减乘除
+    :param arg:
+    :return:
+    '''
 
-class Checknum_Class(object):
+    #乘除
+    arg = common_sub(arg)
 
-    def __init__(self,nums):
-        self.nums = nums
+    #计算加减
+    arg = common_add_sub(arg)
 
-
-    def Remove_parentheses(self):
-        num_search = self.nums
-        while True:
-            check_parentheses = re.search('\([^()]+\)',num_search)
-            if check_parentheses:
-                op_parentheses = re.sub('[()]','',check_parentheses.group())
-                self.Check_operation(op_parentheses)
-                break
-            else:
-                break
+    return arg
 
 
 
+def main_info(numbers):
+    inapp = re.search('\(([\+\-\*\/]*\d+\.?\d*){2,}\)',numbers)
+    if not inapp:
+        args = common_all_div(numbers)
+        return args
 
-    def Check_operation(self,opt):
-        nowopt = re.sub('\s+','',opt)
-        check_opt = re.findall('(\+\-|\+\+|\+|\-|\*|\/)',nowopt)
-        print(check_opt)
+    str_sub = inapp.group()
+    ret = str_sub[1:len(str_sub)-1]
+    args = common_all_div(ret)
+    numbers = numbers.replace(str_sub,args)
+    return main_info(numbers)
 
 
-    def Cals(self):
-        pass
 
+if __name__ == '__main__':
 
-if __name__  == '__main__':
-    #nums = '1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )'
-    nums = '1-2*(1+2*2/4+2*1)'
-
-    numbers = Checknum_Class(nums)
-    numbers.Remove_parentheses()
+    input_num ="1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
+    #input_num = "1-2*-30/-12*(-20+200*-3/-200*-300-100)"
+    #input_num = "1-2-(3.3/5-12*20)"
+    innum = re.sub('\s+','',input_num)
+    args = main_info(innum)
+    print('The end result >>>',args)
