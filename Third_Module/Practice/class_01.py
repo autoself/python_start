df23851d37d6b2581f7cefcde3397d48ed8f6537
autoself <459804062@qq.com>
2017-09-14 09:52:53 +0800

    20170914

--- Third_Module/Practice/class_01.py
+++ Third_Module/Practice/class_01.py
@@ -7,4 +7,10 @@ __date__ = '17-9-14 上午9:43'
 
 
 class Check_Obj(object):
-    
+    def __init__(self,name,age,moot):
+        self.name = name
+        self.age = age
+        self.moot = moot
+        self.__blood = 100
+
+        
