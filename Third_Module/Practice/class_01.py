cde7dc6eba40edafbd69808db66375b6dd1ab2cf
autoself <459804062@qq.com>
2017-09-14 16:53:29 +0800

    20170914

--- Third_Module/Practice/class_01.py
+++ Third_Module/Practice/class_01.py
@@ -13,4 +13,22 @@ class Check_Obj(object):
         self.moot = moot
         self.__blood = 100
 
-        
+    def get_moot(self):
+        print('this is moot: ',self.moot)
+
+    def get_pri(self):
+        print('this is __blood: ',self.__blood)
+
+
+    def change_name(self,bask):
+        print('change is bask: ', bask)
+        self.moot = bask
+
+
+a = Check_Obj('LiMing','18','AK1')
+a.get_moot()
+a.get_pri()
+a.change_name('A8')
+
+print(str(a._Role__blood))
+a.get_moot()
