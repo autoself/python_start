07ce70a66e0f136178606ed02d547030d9be6443
autoself <459804062@qq.com>
2017-09-19 09:22:51 +0800

    Third_v1

---
+++ Third_Module/Practice/class_01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+__author__ = 'andylin'
+__date__ = '17-9-14 上午9:43'
+
+
+
+class Check_Obj(object):
+    def __init__(self,name,age,moot):
+        self.name = name
+        self.age = age
+        self.moot = moot
+        self.__blood = 100
+
+    def get_moot(self):
+        print('this is moot: ',self.moot)
+
+    def get_pri(self):
+        print('this is __blood: ',self.__blood)
+
+
+    def change_name(self,bask):
+        print('change is bask: ', bask)
+        self.moot = bask
+
+
+a = Check_Obj('LiMing','18','AK1')
+a.get_moot()
+a.get_pri()
+a.change_name('A8')
+
+print(str(a._Role__blood))
+a.get_moot()
+
+print('aaa')
