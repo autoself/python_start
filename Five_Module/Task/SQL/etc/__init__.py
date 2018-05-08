3817dbd9755704f1418585aa05c372b9a26d46b3
root <root@localhost.localdomain>
2018-05-08 22:46:02 +0800

    sql_v1

---
+++ Five_Module/Task/SQL/etc/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python env 
+#-*- coding:utf8 -*- 
+
+__author__ = 'andylin'
+__date__ = '18-5-8 下午10:00'
---
+++ Five_Module/Task/SQL/etc/setting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python env 
+#-*- coding:utf8 -*- 
+
+__author__ = 'andylin'
+__date__ = '18-5-8 下午9:59'
+
+import sqlalchemy
